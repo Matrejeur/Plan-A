1054b66a20059c2c48c49cfa83181b51d1d85eee
Matrejeur <52937815+Matrejeur@users.noreply.github.com>
2022-03-18 16:21:28 +0000

    Update and rename vps.tf to vpc.tf

--- Assignment2/vpc.tf
+++ Assignment2/vpc.tf
@@ -11,7 +11,7 @@ locals {
 module vpc {
     source = "terraform-aws-modules/vpc/aws"
     version = "3.2.0"
-    name = "Demo-VPC"
+    name = "DevopsChallenge-VPC"
     cidr = "10.0.0.0/16"
     azs = data.aws_availability_zones.available.names
     private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
