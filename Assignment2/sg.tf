8a65e3f5fa595ac72507d88087f26bc82f0df2f0
Matrejeur <52937815+Matrejeur@users.noreply.github.com>
2022-03-20 22:34:04 +0000

    Update sg.tf

--- Assignment2/sg.tf
+++ Assignment2/sg.tf
@@ -10,6 +10,24 @@ resource "aws_security_group" "sg1" {
         cidr_blocks = [
             "10.0.0.0/8"
         ]
+        
+        {
+      description      = "HTTP from VPC"
+      from_port        = 80
+      to_port          = 80
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+    },
+        
+        {
+      description      = "Application Port"
+      from_port        = 8010
+      to_port          = 8010
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+    },
     }
 }
 
@@ -27,14 +45,16 @@ resource "aws_security_group" "sg2" {
         ]
     }
 }
+    
+    
 
 resource "aws_security_group" "sg3" {
     name_prefix = "sg3"
     vpc_id = module.vpc.vpc_id
 
     ingress {
-        from_port = 22
-        to_port = 22
+        from_port = 443
+        to_port = 443
         protocol = "tcp"
 
         cidr_blocks = [
